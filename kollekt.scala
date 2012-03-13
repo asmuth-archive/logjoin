d789403214c79cc3e007b8b00c1c982bff7bfe8b
Paul Asmuth <paul@paulasmuth.com>
2012-03-13 23:06:30 +0100

    receive data via udp

--- kollekt.scala
+++ kollekt.scala
@@ -1,22 +1,39 @@
-object Kollekt extends Application{
+import scala.actors.Actor
+import scala.actors.Actor._
+import java.net.DatagramPacket 
+import java.net.DatagramSocket
 
-  import scala.actors.Actor
-  import scala.actors.Actor._
+object Kollekt{
 
-  val listener = new Listener
+  val dispatcher = new Dispatcher
 
-  listener ! "fubar|||mydata1"
-  listener ! "fubar|||mydata2"
-  listener ! "fubar|||mydata3"
-  listener ! "fubar|||mydata4"
+  def main(args : Array[String]) : Unit = 
+    listen("localhost", 2323)
 
-  listener.start
+  def listen(host: String, port: Int) = {
+    val sock = new DatagramSocket(port) 
 
-  println("fu")
+    // buffers for receiving data, default size is 1024
+    val buffer_size = 1024
+    val buffer = new Array[Byte](buffer_size) 
+    val packet = new DatagramPacket(buffer, buffer_size)  
 
-  while (true) {
-    //println("sleep")
+    while (true) { 
+      // block until the next packet was received
+      sock.receive(packet)
+
+      // process the received packet
+      received(packet)
+    } 
+  }
+
+  def received(packet: DatagramPacket) = {
+    // cast the received data to a string
+    val data_str = new String(packet.getData(), 0, packet.getLength())
+
+    println("received -> " + data_str) 
   }
 
 }
+
 
