2aabba7377a5b54a229cdbe08bd752acab8255db
Paul Asmuth <paul@paulasmuth.com>
2012-03-14 00:48:37 +0100

    configuration / stats objects

--- Kollekt.scala
+++ Kollekt.scala
@@ -2,8 +2,20 @@ import scala.actors.Actor
 import scala.actors.Actor._
 import java.net.DatagramPacket 
 import java.net.DatagramSocket
+import scala.collection.mutable.HashMap
 
 object Kollekt{
+
+  val config = HashMap[String, Int](
+    "bucket_timeout" -> 600,  // buckets time out after 10 minutes of inactivity
+    "bucket_maxsize" -> 50,   // max 50 items per bucket
+    "bucket_maxage" -> 3600   // buckets time out after one hour
+  )
+
+  val stats = new HashMap[String, Int].withDefault(
+    (s: String) => 0
+  )
+
 
   val dispatcher = new Dispatcher
   dispatcher.start
