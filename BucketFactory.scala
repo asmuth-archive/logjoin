97a9cbd789927b3389b5a79ac7d7bd46e9daf413
Paul Asmuth <paul@paulasmuth.com>
2012-03-13 23:47:25 +0100

    bucketfactory: stateful buckets

--- BucketFactory.scala
+++ BucketFactory.scala
@@ -5,14 +5,20 @@ object BucketFactory { // EPIC WIN FTW! :)
   val buckets = HashMap[String, Bucket]()
 
   def get(bucket_id: String) : Bucket = {
-    // buckets(bucket_id)
-    val bucket = new Bucket(bucket_id)
-    bucket.start
-    return bucket
+    if (buckets.isDefinedAt(bucket_id))
+      buckets(bucket_id)
+    else
+      create(bucket_id)
   }
 
-  def kill(bucket_id: String){
+  def kill(bucket_id: String) = {
     // suicide function / callback
+  }
+
+  private def create(bucket_id: String) : Bucket = {
+    buckets += ((bucket_id, new Bucket(bucket_id)))
+    buckets(bucket_id).start
+    buckets(bucket_id)
   }
 
 }
